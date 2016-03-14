8658fffd2c725d5093a3bdf4ba8a0034dd63dc24
Julian Guterman <gutermanj@gmail.com>
2016-03-13 23:15:17 -0400

    Makin it fancy with some jquery

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,19 +1,24 @@
 $(document).on('ready', function() {
 	loadPage();
 });
+
+
 setTimeout( function loadPage() {
-	$('body').fadeIn(1500);
-	$('body').removeClass('hidden');
-	headerContentTopFadeIn();
-	headerContentBottomFadeIn();
-}, 1000);
+	$('.loading').fadeOut( 200 );
+	setTimeout( function() {
+		$('.main-container').fadeIn(1500);
+		$('.main-container').removeClass('hidden');
+		headerContentTopFadeIn();
+		headerContentBottomFadeIn();
+	}, 500);
+}, 3500);
 
 setTimeout( function headerContentTopFadeIn() {
 	$('.header-content-top').removeClass('hidden');
 	$('.header-content-top').addClass('animated fadeInRight');
-}, 0);
+}, 2500);
 
 setTimeout( function headerContentBottomFadeIn() {
 	$('.header-content-bottom').removeClass('hidden');
 	$('.header-content-bottom').addClass('animated fadeInRight');
-}, 2000);
+}, 5000);
